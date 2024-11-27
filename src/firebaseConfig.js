766aebfd59fa04ef7f5b391937f832e5d0718380
Code <Code@macbookpro-2.home>
2024-11-27 19:04:56 +0100

    Write Firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCf30MKY-WZqSZRrmkeU5bSLRpjzPyB-d8",
+  authDomain: "task-timer-panel.firebaseapp.com",
+  databaseURL:
+    "https://task-timer-panel-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "task-timer-panel",
+  storageBucket: "task-timer-panel.firebasestorage.app",
+  messagingSenderId: "32657303932",
+  appId: "1:32657303932:web:af2e4200dc83e8163ddcf9",
+};
+
+const app = initializeApp(firebaseConfig);
--- src/providers/FireBaseAPI.js
+++ src/providers/FireBaseAPI.js
@@ -1,3 +1,6 @@
+import app from "../firebaseConfig";
+import { getDatabase, ref, set, push } from "firebase/database";
+
 class FetchDataAPI {
   constructor() {
     this.url = `https://task-timer-panel-default-rtdb.europe-west1.firebasedatabase.app/`;
